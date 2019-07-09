f398eb78e07c207e2800cd1eebd9adb410115b41
afroz <afroz@DESKTOP-CSID7BC>
2019-07-09 18:39:25 +0530

    hi Service Profiles Apps

---
+++ src/main/java/org/app/SpringBoot18ServicebaseProfilesExApplication.java
@@ -0,0 +1,13 @@
+package org.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot18ServicebaseProfilesExApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBoot18ServicebaseProfilesExApplication.class, args);
+	}
+
+}
---
+++ src/main/java/org/app/runner/EmailService.java
@@ -0,0 +1,17 @@
+package org.app.runner;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Component;
+
+@Component
+@Profile("email")
+public class EmailService implements CommandLineRunner {
+
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("Email Service");
+
+	}
+
+}
